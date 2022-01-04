9a9ca2f0f690e3a1b2310e73beb1bb231d4e38e3
Lucas Maximiano <lucas97lopes@hotmail.com>
2022-01-03 21:44:41 -0300

    modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -8,7 +8,7 @@ import { Container, RadioBox, TransactionTypeContainer } from "./styles"
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
-import { api } from "../../services/api"
+import { useTransactions } from "../../hooks/useTransactions"
 
 Modal.setAppElement('#root')
 
@@ -18,21 +18,28 @@ interface INewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: INewTransactionModalProps) {
+  const { createTransaction } = useTransactions()
+
   const [title, setTitle] = useState('')
-  const [value, setValue] = useState(0)
+  const [ammount, setAmmount] = useState(0)
   const [category, setCategory] = useState('')
   const [type, setType] = useState('deposit')
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault()
 
-    const data = {
-      value,
+    await createTransaction({
       title,
-      category
-    }
+      ammount,
+      category,
+      type
+    })
 
-    api.post('/transactions', data)
+    setTitle('')
+    setAmmount(0)
+    setCategory('')
+    setType('deposit')
+    onRequestClose()
   }
 
   return (
@@ -60,8 +67,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }: INewTransactionM
           placeholder="Titulo"
         />
         <input
-          value={value}
-          onChange={event => setValue(Number(event.target.value))}
+          value={ammount}
+          onChange={event => setAmmount(Number(event.target.value))}
           type="number"
           placeholder="Valor"
         />
